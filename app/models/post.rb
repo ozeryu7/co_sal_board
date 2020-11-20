0d66b1a04ae63c357f5b4a3b644e491f163bd1b1
ozeryu7 <cr7jpzeo@gmail.com>
2020-11-21 08:11:48 +0900

    create.model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  # has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+
+  validates :title, presence: true, length: {maximum: 140 }
+  validates :content, length: { maximum: 3000 }
 end
