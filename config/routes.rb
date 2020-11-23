a1f2836884feb97e868756ddb7b77c9bf3c94fda
ozeryu7 <cr7jpzeo@gmail.com>
2020-11-23 17:16:40 +0900

    add email login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
             passwords: "users/passwords",
           }
            
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
