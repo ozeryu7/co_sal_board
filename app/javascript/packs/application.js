a5c3ab75f55f58c0584c4f0a4ca416ab40a0c36f
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-03 16:09:42 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,12 @@ import '@fortawesome/fontawesome-free/js/all';
 // that code so it'll be compiled.
 
 //= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require_tree .
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
