e0d37dbddd8df9ff1c5f0ac765454529d28c7846
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-04 17:37:25 +0900

    fix post for active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,22 +2,22 @@ class Post < ApplicationRecord
   # has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :user
-  has_many_attached :images
+  has_one_attached :image
 
   validate :image_type
   validates :title, presence: true, length: {maximum: 140 }
   validates :content, length: { maximum: 3000 }
 
     def thumbnail input
-      return self.image[input].variant(resize: '300x300!').processed
+      return self.images[input].variant(resize: '300x300!').processed
     end
 
     private
 
         def image_type
-          if images.each do |image|
+          if image.each do |image|
             if !image.content_type.in?(%('image/jpec image/png image/jpg image/jpeg'))
-              errors.add(:images, 'ファイルが対応している画像データではありません')
+              errors.add(:image, 'ファイルが対応している画像データではありません')
             end
           end
         end
