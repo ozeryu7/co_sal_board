91dcdae2a20778366403d4f701b1f06253a9e005
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-05 23:01:06 +0900

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  # has_many :likes, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_one_attached :image
