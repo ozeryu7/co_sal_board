4f6757ea8e07e7e06f2162b5608cecb3bc4b0ddf
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-09 19:51:14 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,15 @@
 class LikesController < ApplicationController
   def create
     Like.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to posts_path
+  end
+
+  def destroy
+    Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
+  end
+
+  private
+
+  def post_params
+    @post = Post.find(params[:id])
   end
 end
