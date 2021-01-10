959452e223814e2060362267020e2f645cb1df52
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-10 15:56:12 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed/id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def liked_by?(post_id)
     likes.where(post_id: post_id).exists?
@@ -29,5 +38,25 @@ class User < ApplicationRecord
       user.save(validate: false) 
     end
     user
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  #ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  #現在のユーザーがフォローしていたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  #現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 end
