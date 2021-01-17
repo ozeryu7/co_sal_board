066cef729f36699a97c48c2d8d57a5be6aeef403
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-17 23:37:18 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # before_save :downcase_email
+  # validates :name, presence: true, length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  #                   format: { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: { case_sensitive: false }
+  # has_secure_password
+  # validates :password, presence: true, length: { minimum: 6 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
@@ -59,4 +67,11 @@ class User < ApplicationRecord
   def followed_by?(other_user)
     followers.include?(other_user)
   end
+
+  # private
+
+  # def downcase_email
+  #   self.email = email.downcase
+  # end
+
 end
