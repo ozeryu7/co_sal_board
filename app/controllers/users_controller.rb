195402d53885da1dfc16f8d5046b60abc739d927
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-17 23:37:12 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  def index
+  end
+ 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
