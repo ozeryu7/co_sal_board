84ee024b3360b1927ae45f0c5ff139255cddc7a9
ozeryu7 <cr7jpzeo@gmail.com>
2021-01-22 22:13:08 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it "ユーザーの名前が取得できること" do
-    user = User.new(name: "satou")
-    expect(user.disp_name).to eq "satouさん"
-  end
-end
+# RSpec.describe User, type: :model do
+#   it "ユーザーの名前が取得できること" do
+#     user = User.new(name: "satou")
+#     expect(user.disp_name).to eq "satouさん"
+#   end
+# end
 
