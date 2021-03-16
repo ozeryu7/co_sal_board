9824982a8c936b6b65869fc7039e100cf70c1173
ozeryu7 <cr7jpzeo@gmail.com>
2021-03-16 17:10:02 +0900

    [fix]デプロイ前バグの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => host,
     :authentication => :plain,
+    }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
